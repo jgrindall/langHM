1e085cea2e391823b6746283b781ea9ee2b63739
jgrindall <john.grindall@gmail.com>
2012-10-08 16:29:16 +0100

    adding libs

--- bat/build.bat
+++ bat/build.bat
@@ -2,4 +2,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 set lib=..\langHMLib\langHMLib.swc
 
-compc -source-path ..\langHM\src -include-sources ..\langHM\src -external-library-path+="%lib%" -output ..\langHM\langHM.swc
+set tlf=..\langHM\lib\tlf.swc
+
+set fonts=..\langHM\lib\fonts.swc
+
+compc -source-path ..\langHM\src -include-sources ..\langHM\src -external-library-path+="%lib%" -external-library-path+="%tlf%" -external-library-path+="%fonts%" -output ..\langHM\langHM.swc
